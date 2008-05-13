ea782f9a1ea2563481fb5e26d8e4cab690538b73
Ryan Lowe <ryanlowe@gmail.com>
2008-05-13 17:20:55 -0400

    add support for seconds

--- public/javascripts/client_date.js
+++ public/javascripts/client_date.js
@@ -2,8 +2,8 @@
 var weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-function fdt(y,m,d,hr,mn) {
-  gmtDate = new Date(Date.UTC(y,m-1,d,hr,mn));
+function fdts(y,m,d,hr,mn,s) {
+  gmtDate = new Date(Date.UTC(y,m-1,d,hr,mn,s));
 	text = months[gmtDate.getMonth()];
   text += ' '+gmtDate.getDate();
   ampm = 'AM';
@@ -12,14 +12,33 @@ function fdt(y,m,d,hr,mn) {
   if (hours == 0) { hours = 12; }
   if (hours > 12) { hours -= 12; }
   text += ' '+hours;
-  minutes = gmtDate.getMinutes()
+  minutes = gmtDate.getMinutes();
+  if (minutes < 10) { minutes = '0'+minutes; }
+  text += ':'+minutes;
+  seconds = gmtDate.getSeconds();
+  if (seconds < 10) { seconds = '0'+seconds; }
+  text += ':'+seconds;
+  text += ' '+ampm;
+  document.write(text);
+}
+function fdt(y,m,d,hr,mn,s) {
+  gmtDate = new Date(Date.UTC(y,m-1,d,hr,mn,s));
+	text = months[gmtDate.getMonth()];
+  text += ' '+gmtDate.getDate();
+  ampm = 'AM';
+  hours = gmtDate.getHours();
+  if (hours > 11) { ampm = 'PM'; }
+  if (hours == 0) { hours = 12; }
+  if (hours > 12) { hours -= 12; }
+  text += ' '+hours;
+  minutes = gmtDate.getMinutes();
   if (minutes < 10) { minutes = '0'+minutes; }
   text += ':'+minutes;
   text += ' '+ampm;
   document.write(text);
 }
-function fd(y,m,d,hr,mn) {
-  gmtDate = new Date(Date.UTC(y,m-1,d,hr,mn));
+function fd(y,m,d,hr,mn,s) {
+  gmtDate = new Date(Date.UTC(y,m-1,d,hr,mn,s));
   text = months[gmtDate.getMonth()];
   text += ' '+gmtDate.getDate();
   document.write(text);
